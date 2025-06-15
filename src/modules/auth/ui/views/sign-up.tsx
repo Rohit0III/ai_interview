56e83a29a16941c0195e8581e76f88c504867b4a
Rohit0III <rohitparik172003@gmail.com>
2025-06-15 14:09:32 +0530

    06:dashboard-sidebar

--- src/modules/auth/ui/views/sign-up.tsx
+++ src/modules/auth/ui/views/sign-up.tsx
@@ -250,7 +250,7 @@ export const SignUpView = () => {
                     </Form>
 
 
-                    <div className="bg-radial from-[#00094B] to-[#0C0C0C] relative hidden md:flex flex-col 
+                    <div className="bg-radial from-sidebar-accent to-sidebar relative hidden md:flex flex-col 
                     gap-y-4 items-center justify-center">
                         <img src="/logo.png" alt="Image" className="h-[92px] w-[92px]" />
                         <p className="text-2xl font-semibold text-white text-shadow-2xs uppercase">
