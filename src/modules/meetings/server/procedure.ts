cac5970c2f86069e733a8c064516e13bea523d59
Rohit0III <rohitparik172003@gmail.com>
2025-07-30 15:37:57 +0530

    17: MEetings-Form

--- src/modules/meetings/server/procedure.ts
+++ src/modules/meetings/server/procedure.ts
@@ -6,11 +6,53 @@ import z from "zod"
 import { and, desc, count, eq, getTableColumns, ilike } from "drizzle-orm"
 import { DEFAULT_PAGE, DEFAULT_PAGE_SIZE, MAX_PAGE_SIZE, MIN_PAGE_SIZE } from "@/constants"
 import { TRPCError } from "@trpc/server"
+import { meetingsInsertSchema, meetingsUpdateSchema } from "../schemas"
 
 
 
 
 export const meetingsRouter = createTRPCRouter({
+
+    update: protectedProcedure
+      .input(meetingsUpdateSchema)
+      .mutation(async ({ ctx, input }) => {
+        const [updatedMeeting] = await db
+          .update(meetings)
+          .set(input)
+          .where(
+            and(
+              eq(meetings.id, input.id),
+              eq(meetings.userId, ctx.auth.user.id),
+            )
+          )
+          .returning();
+  
+          if(!updatedMeeting){
+            throw new TRPCError({
+              code:"NOT_FOUND",
+              message:"Meeting not Found"
+            })
+          }
+  
+          return updatedMeeting;
+      }),
+  
+
+    create: protectedProcedure
+        .input(meetingsInsertSchema )
+        .mutation(async ({ input, ctx }) => {
+          const [createdMeeting] = await db.insert(meetings).values(
+            {
+              ...input,
+              userId: ctx.auth.user.id, // Ensure the correct property is set
+            }
+          )
+            .returning();
+
+            // TODO: Create Stream Call , Upset Stream Users
+    
+          return createdMeeting;
+        }),
 
   // TODO: Change "getOne" to use "proctectedProcedure"
   getOne: protectedProcedure
