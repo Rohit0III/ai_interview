6a2a57aa7a5610dfe389fbce111babc3a014f74c
Rohit0III <rohitparik172003@gmail.com>
2025-08-14 17:50:46 +0530

    24: Background Inngest Jobs

--- src/app/api/webhook/route.ts
+++ src/app/api/webhook/route.ts
@@ -3,10 +3,10 @@ import {and , eq, not } from "drizzle-orm"
 import { NextRequest,NextResponse } from "next/server"
 
 import {
-    // CallEndedEvent,
-    // CallTranscriptionReadyEvent,
-    // CallSessionParticipantJoinedEvent,
-    // CallRecordingReadyEvent,
+    CallEndedEvent,
+    CallTranscriptionReadyEvent,
+    CallSessionParticipantJoinedEvent,
+    CallRecordingReadyEvent,
     CallSessionStartedEvent,
     CallSessionParticipantLeftEvent,
 } from "@stream-io/node-sdk"
@@ -14,6 +14,8 @@ import {
 import { db } from "@/Database"
 import { agents,meetings } from "@/Database/schema"
 import {streamVideo} from "@/lib/stream-video"
+import { inngest } from "@/inngest/client"
+
 
 
 
@@ -116,6 +118,68 @@ export async function POST(req:NextRequest){
             await call.end()
 
         }
+
+        else if ( eventType === "call.session_ended") {
+            const event = payload as CallEndedEvent
+            const meetingId = event.call.custom?.meetingId
+
+            if(!meetingId) {
+                return NextResponse.json({error:"Missing MeetingId"},{status: 400})
+            }
+
+            await db
+            .update(meetings)
+            .set({
+                status:"processing",
+                endedAt:new Date(),
+            }) 
+            .where(and(eq(meetings.id, meetingId), eq(meetings.status, "active")))
+        }
+
+        else if (eventType === "call.transcription_ready") {
+            const event = payload as CallTranscriptionReadyEvent
+            const meetingId = event.call_cid.split(":")[1] // call_cid is fromatted as "type: id"
+
+            const [updatedMeeting] = await db 
+            .update(meetings)
+            .set({
+                transcriptUrl: event.call_transcription.url
+            })
+            .where(eq(meetings.id ,meetingId))
+            .returning();
+
+            if(!updatedMeeting){
+                return NextResponse.json({error:"Meeting not Found "},{status:404})
+            }
+
+            // TODO : Call Inngest background job to summarize the transcript 
+
+            await inngest.send(
+                {
+                    name:"meetings/processing",
+                    data:{
+                        meetingId:updatedMeeting.id,
+                        transcriptUrl:updatedMeeting.transcriptUrl
+                    }
+                }
+            )
+        }
+
+
+         else if (eventType === "call.recording_ready") {
+            const event = payload as CallRecordingReadyEvent
+            const meetingId = event.call_cid.split(":")[1] // call_cid is fromatted as "type: id"
+
+            await db 
+            .update(meetings)
+            .set({
+                recordingUrl: event.call_recording.url
+            })
+            .where(eq (meetings.id , meetingId))
+
+        }
+
+
 
     return NextResponse.json({status :"ok"});
 }
