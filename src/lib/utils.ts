bea68e7821f5324501024f9ec611d537644e9eff
Rohit0III <rohitparik172003@gmail.com>
2025-08-15 13:37:33 +0530

    25: completed-State

--- src/lib/utils.ts
+++ src/lib/utils.ts
@@ -1,6 +1,18 @@
 import { clsx, type ClassValue } from "clsx"
+import humanizeDuration from "humanize-duration"
 import { twMerge } from "tailwind-merge"
 
 export function cn(...inputs: ClassValue[]) {
   return twMerge(clsx(inputs))
+}
+
+export function formatDuration(seconds:number){
+    return humanizeDuration(
+        seconds *1000, // Convert seconds to milliseconds
+        {
+            largest:1,
+            round: true,
+            units: ["h", "m", "s"],
+        }
+    )
 }
