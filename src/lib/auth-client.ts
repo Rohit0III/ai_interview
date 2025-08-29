b089ecc0e92775ea7c1bfb0bf1dd7ee7003c9cb8
Rohit0III <rohitparik172003@gmail.com>
2025-08-29 11:55:45 +0530

    testing

--- src/lib/auth-client.ts
+++ src/lib/auth-client.ts
@@ -1,7 +1,8 @@
 import { polarClient } from "@polar-sh/better-auth"
 import { createAuthClient } from "better-auth/react"
 export const authClient = createAuthClient({
-    baseURL: process.env.BETTER_AUTH_URL! || "http://localhost:3000",
+    // baseURL: process.env.BETTER_AUTH_URL! || "http://localhost:3000",
+    baseURL:"https://ai-interview-pink-xi.vercel.app",
 
     plugins:[polarClient(),],
 })
